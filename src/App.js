ffdefe6f8062ddd3a1e5ab742778b41496accb23
Vinojan Iruthayam <vinojaniruthayam@Vinos-MacBook-Pro.local>
2020-02-20 11:58:35 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokeSetUp: [],
+      isLoading: true,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then(res =>
+        res.data.map(joke => ({
+          setup: `${joke.setup}`,
+          punchline: `${joke.punchline}`,
+          id: `${joke.id}`
+        }))
+      )
+      .then(jokeSetUp => {
+        this.setState({
+          jokeSetUp,
+          isLoading: false
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { isLoading, jokeSetUp } = this.state
+    return (
+      <>
+      <div className="App">
+        <header className="title">
+          App to fetch a string
+          </header>
+        <div>
+          {!isLoading ? (
+            jokeSetUp.map(joke => {
+              const { setup, punchline, id } = joke;
+              console.log(setup, punchline);
+              return (
+                <div key={id} className="content">
+                  <p className="content">{setup}</p>
+                  <p className="content">{punchline}</p>
+                </div>
+              );
+            })
+          ) : (
+              <p>Loading...</p>
+            )}
+        </div>
+      </div>
+      </>
+    );
+  };
+};
+
+/*
+const Joke = (props) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="content">{props.setup}</div>
+    </>
+  )
 }
+*/
 
 export default App;
